320c47f7f523c6967f291d05a4045fa9c0a5eaa1
Chao Xu <cxu24@mylangara.ca>
2022-03-28 16:57:28 -0700

    comment out one line from app.js

--- app.js
+++ app.js
@@ -4,4 +4,4 @@ const server = app.listen(process.env.PORT || 8080, ()=>console.log("Listening")
 
 app.use(express.static("public"));
 app.use(express.urlencoded({extended:true}));
-app.use(express.json());
+// app.use(express.json());
