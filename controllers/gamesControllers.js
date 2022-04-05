fb3aded2fbbf7741573ec6b96f8c5fcf4688010a
Chao Xu <cxu24@mylangara.ca>
2022-04-04 17:57:05 -0700

    new version of api

--- controllers/gamesControllers.js
+++ controllers/gamesControllers.js
@@ -15,7 +15,7 @@ const getGamesFromCompany = (req,res)=>{
         res.status(200).send(result);
     })
     .catch(error=>res.status(500).send(error));
-}
+};
 
 const getOneHighScoreGame = (req,res)=>{
     Game.aggregate([{$match:{score:{$gte:7}}}, {$group:{_id:null}}]).exec()
@@ -26,32 +26,36 @@ const getOneHighScoreGame = (req,res)=>{
 };
 
 const postGame = (req,res)=>{
-    Company.findOne({"name": req.body.company}).exec()
-    .then(company=>{
-        console.log(company);
-        let game = new Game({
-            name: req.body.name,
-            developer: req.body.company,
-            platforms: req.body.platforms,
-            score: req.body.score
-        });
-        company.games.push(game);
-        company.save()
-        .then(result=>{
-            console.log(result.data);
-        })
-        .catch(error=>res.status(500).send(error))
-        game.save()
-        .then(result=>{
-            let newGame = ({
-                data: game,
-                url: `/GameAPI/locations/${game._id}`
+     if (res.locals.errors != null) {
+        res.send(res.locals.errors);
+    } else {
+        Company.findOne({"name": req.body.company}).exec()
+        .then(company=>{
+            console.log(company);
+            let game = new Game({
+                name: req.body.name,
+                developer: req.body.developer,
+                platforms: req.body.platforms,
+                score: req.body.score
             });
-            res.set('content-location', `/GameAPI/locations/${game._id}`);
-            res.status(201).send(newGame);
+            company.games.push(game);
+            company.save()
+            .then(result=>{
+                console.log(result.data);
+            })
+            .catch(error=>console.log(error));
+            game.save()
+            .then(result=>{
+                let newGame = ({
+                    data: game,
+                    url: `/GameAPI/locations/${game._id}`
+                });
+                res.set('content-location', `/GameAPI/locations/${game    ._id}`);
+                res.status(201).send(newGame);
+            })
+            .catch(error=>console.log(error));
         })
-        .catch(error=>res.status(500).send(error));
-    })
+    }
     
 };
 
--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import React from 'react';
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import HighScoreGame from './HighScoreGame.js';
 import CreateCompany from './CreateCompany.js';
 import CreateGame from './CreateGame.js';
+import axios from 'axios';
 import DisplayGames from './DisplayGames.js';
 
 const App = props=>{
     
     const [companies, setCompanies] = useState();
     
+    useEffect(function displayCompanies(){
+        axios.get('/GameAPI/company')
+        .then(result=>{
+            setCompanies(result.data);
+        })
+        .catch(error=>console.log(error));
+    }, [companies]);
+    
     const handleCompanies = (allCompanies)=>{
         setCompanies(allCompanies);
-    }
+    };
     
     return <>
         <div className="app">
